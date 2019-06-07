e909cee622cb2718f782ec301721bc48beab8939
teej387 <teej387@yahoo.com>
2019-06-06 19:14:51 -0500

    Added Clear button to TodoForm

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -12,6 +12,7 @@ function TodoForm (props) {
           name='task'
         />
         <button type='submit'>Add Task</button>
+        <button type='button'>Clear Completed Tasks</button>
       </form>
     </div>
   ) 
